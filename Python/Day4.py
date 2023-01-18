9a39122adf9c67464358a9b0541af29c670dad10
RevEmmanuel <adeolaae1@gmail.com>
2023-01-18 11:02:00 +0100

    Day 4

---
+++ Python/Day4.py
@@ -0,0 +1,11 @@
+print("You'll be asked a bunch of questions then we'll make you up an amazing story with YOU as the star!")
+print()
+
+name = input("Your name > ")
+enemy = input("Your worst enemy > ")
+super_power = input("What is your super power > ")
+print()
+
+print("\033[31m", "Our story begins as our hero", name, "approaches a foreboding castle...")
+print("Suddenly a bolt of lightning striked the ground at the feet of", name)
+print("Our final battle begins!, shouts the evil", enemy, "clearly missing the fact that", name, "has the power of", super_power, "which means they'll win quite easily.", "\033[0m")
