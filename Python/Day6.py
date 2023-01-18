576d33ff86f415910240443fbd53886436b4c76d
RevEmmanuel <adeolaae1@gmail.com>
2023-01-18 11:27:53 +0100

    Day 6

---
+++ Python/Day6.py
@@ -0,0 +1,17 @@
+print("MY LOGIN SYSTEM")
+print("++++++++++++++++++++")
+username = input("Username: ")
+password = input("Password: ")
+
+if username == "David":
+    print("Why hello there David, what a lovely accent you have, you could have charmed your way in here even without a password.")
+    print()
+    print("Have a great day!")
+    print("Don't forget to wear a hat in the sun!")
+elif username == "Sandra":
+    print("Why hello there Sandra, you look amazing today, you could have charmed your way in here even without a password.")
+    print()
+    print("Have a lovely day!")
+    print("Don't forget to smile!")
+else:
+    print("Sorry, I don't know you.")
