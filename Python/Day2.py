0c6fe627ba57a221d0eb79c08b1bc81d45b1049d
RevEmmanuel <adeolaae1@gmail.com>
2023-01-18 10:36:24 +0100

    Day 2

---
+++ Python/Day2.py
@@ -0,0 +1,8 @@
+name = input("What is your name? ")
+food = input("What is your favorite food? ")
+music = input("What is your favorite music? ")
+location = input("Where do you live? ")
+print()
+
+print(f"You are {name}. You are probably hungry for {food}.")
+print(f"You're definitely getting your groove on to {music} living in the amazing {location}")
