b62ed70fffef0dcd0b4bcb4bc6fb67060e02a405
RevEmmanuel <adeolaae1@gmail.com>
2023-01-18 10:48:59 +0100

    Day 3

---
+++ Python/Day3.py
@@ -0,0 +1,9 @@
+food = input("Name a food > ")
+plant = input("Name a type of plant > ")
+cooking_method = input("Name a method of cooking > ")
+describe_burned_food = input("What word describes burned food? > ")
+item = input("Name a DIY item > ")
+print()
+
+print("Menu")
+print(cooking_method, food, "with", describe_burned_food, plant, "on a bed of", item)
