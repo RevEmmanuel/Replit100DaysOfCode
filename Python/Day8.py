a95bbaecc3ff31ea29d114cfefb7378777fe7371
RevEmmanuel <adeolaae1@gmail.com>
2023-01-20 12:56:49 +0100

    Day 8

---
+++ Python/Day8.py
@@ -0,0 +1,33 @@
+
+print("Hello.")
+name = input("What is your name? ")
+
+
+
+if name =="Emmanuel" or name == "emmanuel":
+ day = input("What is the day of the week?")
+ if day == "monday" or day == "Monday":
+   print("It is going to be a great Monday", name)
+ if day == "tuesday" or day == "Tuesday":
+   print("What a wonderful Tuesday it is", name)
+ if day == "wednesday" or day == "Wednesday":
+   print("Happy Hump Day", name)
+ if day == "thursday" or day == "Thursday":
+   print(name,", your week is almost over!")
+ if day == "friday" or day == "Friday":
+   print(name, "It's FRIDAY! TGIF!")
+
+elif name == "David" or name== "david":
+ day = input("What is the day of the week?")
+ if day == "monday" or day == "Monday":
+   print("It is going to be a great Monday", name)
+ if day == "tuesday" or day == "Tuesday":
+   print("You look great in that color", name)
+ if day == "wednesday" or day == "Wednesday":
+   print("You look chipper today", name)
+ if day == "thursday" or day == "Thursday":
+   print(name,"you are doing a great job!")
+ if day == "friday" or day == "Friday":
+   print(name, "it's FRIDAY!")
+else:
+ print("I do not know your name, but I hope you are having a great day!")
