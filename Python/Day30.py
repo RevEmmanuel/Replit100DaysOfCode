cbe90342f7f8c36466b5705ad3c9ae4986957c0e
RevEmmanuel <adeolaae1@gmail.com>
2023-01-30 23:18:49 +0100

    Day 30

---
+++ Python/Day30.py
@@ -0,0 +1,7 @@
+print("30 Days down, what did you think? ")
+
+for i in range(1, 31):
+    thought = input(f"Day {i}: \n")
+    default_text = f"You thought Day {i} was"
+    print(f"{default_text:^35}")
+    print(f"{thought:^35}")
